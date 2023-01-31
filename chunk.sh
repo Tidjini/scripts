67368f00884630f34ee703630f1841a9d53404b9
tidjini <tidjini.messaoudi@gmail.com>
2023-01-31 22:42:33 +0100

    clean

--- chunk.sh
+++ chunk.sh
@@ -1,21 +1,3 @@
-PASSWORD=$1
-# ##########UPDATE AND UPGRADE (1)###################
-update_and_upgrade(){
-    pass_show="**********"
-    echo "UPDATING AND UPGRADING..."
-    echo "UPDATE..."
-    echo $PASSWORD | sudo -S -k apt-get -y update
-    echo $pass_show
-    echo "UPGRADE..."
-    echo $PASSWORD | sudo -S -k apt-get -y upgrade
-    echo $pass_show
-    echo "UPDATE AND UPGRADE FINISH"
-}
-echo 'testing ...'
-update_and_upgrade
-# ##########UPDATE AND UPGRADE###################
-
-
 # default logs path /home/project_name/.logs
 # USER -> BLOCANTE -> REPORT AND STOP PROCESSING
 # check if the user exist
