1dadbcd119489d303c67772cda7d9670d3add4a1
tidjini <tidjini.messaoudi@gmail.com>
2023-03-04 09:46:08 +0100

    load env file

--- python/environment.py
+++ python/environment.py
@@ -1,6 +1,12 @@
 from decouple import config
 
-#Load env variables from the chosen file
-config_file = input('Enter the name of the environment file you want to use: ')
-config_file_path = f'.env.{config_file}'
-config.read_dotenv(config_file_path)
+def load_env():
+    #Load env variables from the chosen file
+    config_file = input('Enter the name of the environment file you want to use: ')
+    config_file_path = f'.env.{config_file}'
+    config.read_dotenv(config_file_path)
+
+
+
+if __name__ == "__main__":
+    load_env()
