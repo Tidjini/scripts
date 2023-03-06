99535bdba93c28717dd6d6f536baff9dc2741b48
tidjini <tidjini.messaoudi@gmail.com>
2023-03-06 20:50:52 +0100

    OR Query

--- django/orm_cookbook.py
+++ django/orm_cookbook.py
@@ -1,4 +1,12 @@
+from django.db.models import Q
+from application import models
 # find the query associated with a queryset
 queryset = models.Event.objects.all()
 print("SQL Query")
 print(str(queryset.query))
+# OR query
+authorized_event = models.Event.objects.filter(authorize=True)
+confirmed_event = models.Event.objects.filter(confirm=True)
+result = authorized_event | confirmed_event
+# OR with filter Q
+result = models.Event.objects.filter(Q(authorize=True) | Q(confirm=True))
