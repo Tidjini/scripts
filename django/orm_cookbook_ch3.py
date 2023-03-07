cbef56740e4751e06d5233bcea1bf3bb15fb0891
Tidjini <tidjini.messaoudi@gmail.com>
2023-03-07 16:13:01 +0100

    create multiple objects

--- django/orm_cookbook_ch3.py
+++ django/orm_cookbook_ch3.py
@@ -1,1 +1,11 @@
 # Create, update, deleting things
+from app.models import Category
+# create multiple objects
+
+Category.objects.bulk_create(
+    [
+        Category(name="A"),
+        Category(name="B"),
+        Category(name="C"),
+    ]
+)
