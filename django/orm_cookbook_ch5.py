211c03f1a9ad1a79638a729c746dae477821da22
tidjini <tidjini.messaoudi@gmail.com>
2023-03-07 20:30:02 +0100

    Many To Many Field

---
+++ django/orm_cookbook_ch5.py
@@ -0,0 +1,25 @@
+# Database modeling
+
+# Many to Many Relationship
+
+class User(Model):
+    tweet = CharField(max_length=255)
+    follower = ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+    pass
+
+
+user1 = User(tweet="tweet")
+user1.save()
+user2 = User(tweet="tweet user 2")
+user2.save()
+user3 = User(tweet="tweet user 3")
+user3.save()
+
+# Add Follower
+user1.follower.add(user2)
+user1.follower.add(user3)
+user1.save()
+
+# Remove Follower
+user1.follower.remove(user2)
+user1.save()
